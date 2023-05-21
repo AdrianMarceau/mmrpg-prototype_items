8f56eae0711e4e2f4d4289b0650a99f6cf5deaf6
Ageman20XX <adrian.marceau@gmail.com>
2023-05-21 02:04:45 -0400

    Updated the Field Booster's functions

--- field-booster/functions.php
+++ field-booster/functions.php
@@ -19,23 +19,6 @@ $functions = array(
         else { $temp_boost_type = $temp_core_type; }
         if (!isset($this_field->field_multipliers[$temp_boost_type]) || $this_field->field_multipliers[$temp_boost_type] < MMRPG_SETTINGS_MULTIPLIER_MAX){
 
-            // Define this item's attachment token
-            $this_arrow_index = rpg_prototype::type_arrow_image('boost', !empty($temp_boost_type) ? $temp_boost_type : 'none');
-            $this_attachment_token = 'item_effects_field-booster';
-            $this_attachment_info = array(
-                'class' => 'item',
-                'attachment_token' => $this_attachment_token,
-                'item_token' => $this_item->item_token,
-                'item_image' => $this_arrow_index['image'],
-                'item_frame' => $this_arrow_index['frame'],
-                'item_frame_animate' => array($this_arrow_index['frame']),
-                'item_frame_offset' => array('x' => 0, 'y' => 0, 'z' => -10)
-                );
-
-            // Attach this item attachment to this robot temporarily
-            $this_robot->set_frame('taunt');
-            $this_robot->set_attachment($this_attachment_token, $this_attachment_info);
-
             // Create or increase the elemental booster for this field
             $temp_change_percent = round($this_item->get_recovery2() / 100, 1);
             $new_multiplier_value = (isset($this_field->field_multipliers[$temp_boost_type]) ? $this_field->field_multipliers[$temp_boost_type] : 1) + $temp_change_percent;
@@ -46,17 +29,68 @@ $functions = array(
             $this_field->field_multipliers[$temp_boost_type] = $new_multiplier_value;
             $this_field->update_session();
 
-            // Create the event to show this element boost
+            // Only proceed if the field multiplier has actually changed by a positive amount
             if ($temp_change_percent > 0){
+
+                // Collect the elemental type arrow index
+                $this_arrow_index = rpg_prototype::type_arrow_image('boost', !empty($temp_boost_type) ? $temp_boost_type : 'none');
+                $this_types_index = rpg_type::get_index();
+
+                // Collect the type colours so we can use them w/ effects
+                $temp_boost_colour_dark = 'rgb('.implode(', ', $this_types_index[$temp_boost_type]['type_colour_dark']).')';
+                $temp_boost_colour_light = 'rgb('.implode(', ', $this_types_index[$temp_boost_type]['type_colour_light']).')';
+
+                // Define this item's attachment token
+                $this_attachment_token = 'item_effects_field-booster';
+                $this_attachment_info = array(
+                    'class' => 'item',
+                    'attachment_token' => $this_attachment_token,
+                    'item_token' => $this_item->item_token,
+                    'item_image' => $this_arrow_index['image'],
+                    'item_frame' => $this_arrow_index['frame'],
+                    'item_frame_animate' => array($this_arrow_index['frame']),
+                    'item_frame_offset' => array('x' => 0, 'y' => 0, 'z' => -10)
+                    );
+
+                // Define a separate attachment for the background to show a large colour overlay
+                $fx_attachment_token = 'item_effects_field-booster_overlay';
+                $fx_attachment_info = array(
+                    'class' => 'item',
+                    'attachment_token' => $this_attachment_token,
+                    'sticky' => true,
+                    'item_token' => $this_item->item_token,
+                    'item_image' => '_effects/black-overlay',
+                    'item_frame' => 0,
+                    'item_frame_animate' => array(0, 1),
+                    'item_frame_offset' => array('x' => -5, 'y' => 20, 'z' => -12),
+                    'item_frame_classes' => 'sprite_fullscreen ',
+                    'item_frame_styles' => 'opacity: 0.5; filter: alpha(opacity=50); background-image: none; background-color: '.$temp_boost_colour_dark.'; '
+                    );
+
+                // Attach this item and overlay to the robot temporarily
+                $this_robot->set_frame('taunt');
+                $this_robot->set_attachment($this_attachment_token, $this_attachment_info);
+                $this_robot->set_attachment($fx_attachment_token, $fx_attachment_info);
+
+                // Create the event to show this element boost
                 $this_battle->events_create($this_robot, false, $this_field->field_name.' Multipliers',
                     $this_robot->print_name().' triggers '.$this_robot->get_pronoun('possessive2').' '.$this_item->print_name().'!<br />'.
                     'The <span class="item_name item_type item_type_'.$temp_boost_type.'">'.ucfirst($temp_boost_type).'</span> field multiplier rose to <span class="item_name item_type item_type_none">'.$new_multiplier_value.'</span>!',
-                    array('canvas_show_this_item_overlay' => true)
+                    array(
+                        'canvas_show_this_item_overlay' => true,
+                        'event_flag_camera_action' => true,
+                        'event_flag_camera_side' => $this_robot->player->player_side,
+                        'event_flag_camera_focus' => $this_robot->robot_position,
+                        'event_flag_camera_depth' => $this_robot->robot_key
+                        )
                     );
-            }
 
-            // Remove this item attachment from this robot
-            $this_robot->unset_attachment($this_attachment_token);
+                // Remove this item attachment from this robot
+                $this_robot->reset_frame();
+                $this_robot->unset_attachment($this_attachment_token);
+                $this_robot->unset_attachment($fx_attachment_token);
+
+            }
 
         }
 
