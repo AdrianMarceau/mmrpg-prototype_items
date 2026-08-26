4dffb3a3dbc205c6158852bd1f1bf83f404898c9
Ageman20XX <adrian.marceau@gmail.com>
2026-08-26 16:58:50 -0400

    Updated the Uptick Module's data and functions

--- uptick-module/functions.php
+++ uptick-module/functions.php
@@ -1,18 +1,54 @@
 <?
 $functions = array(
     'item_function' => function($objects){
-
-        // Extract all objects into the current scope
-        extract($objects);
-
-        // Generate an event to show nothing happened
-        $event_header = $this_robot->robot_name.'&#39;s '.$this_item->item_name;
-        $event_body = 'Nothing happened&hellip;';
-        $this_battle->events_create($this_robot, $target_robot, $event_header, $event_body, array('this_item' => $this_item));
-
-        // Return true on success
         return true;
-
+    },
+    'item_function_onload' => function($objects){
+        extract($objects);
+        $this_item->priority = -5;
+        return true;
+    },
+    'rpg-ability_stat-boost_before' => function($objects){
+        extract($objects);
+        
+        // If this robot is not the one initiating the stat change, ignore
+        if ($this_robot !== $initiator_robot){ return false; }
+        
+        // If the boost is not a fixed amount, we can amplify it
+        if (!$options->is_fixed_amount && $options->boost_amount != 0){
+            $trigger_text = $this_robot->print_name().'\'s '.$this_item->print_name().' amplified the stat boost! ';
+            if (!empty($options->extra_text)){ $options->extra_text .= ' <br /> '; }
+            $options->extra_text .= $trigger_text;
+            
+            // Add 1 stage of boost, respecting whether it's currently positive or negative
+            if ($options->boost_amount > 0){
+                $options->boost_amount += 1;
+            } elseif ($options->boost_amount < 0){
+                $options->boost_amount -= 1;
+            }
+        }
+        return true;
+    },
+    'rpg-ability_stat-break_before' => function($objects){
+        extract($objects);
+        
+        // If this robot is not the one initiating the stat change, ignore
+        if ($this_robot !== $initiator_robot){ return false; }
+        
+        // If the break is not a fixed amount, we can amplify it
+        if (!$options->is_fixed_amount && $options->break_amount != 0){
+            $trigger_text = $this_robot->print_name().'\'s '.$this_item->print_name().' amplified the stat break! ';
+            if (!empty($options->extra_text)){ $options->extra_text .= ' <br /> '; }
+            $options->extra_text .= $trigger_text;
+            
+            // Add 1 stage of break, respecting whether it's currently positive or negative
+            if ($options->break_amount > 0){
+                $options->break_amount += 1;
+            } elseif ($options->break_amount < 0){
+                $options->break_amount -= 1;
+            }
+        }
+        return true;
     }
 );
 ?>
